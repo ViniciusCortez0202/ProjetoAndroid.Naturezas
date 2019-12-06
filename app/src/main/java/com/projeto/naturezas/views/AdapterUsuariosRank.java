125e0822a4d0d5367adaaf3b5889cf8c6236510d
unknown <cortezvinicius11@gmail.com>
2019-12-06 10:01:34 +0000

    teste adapter

--- app/src/main/java/com/projeto/naturezas/views/AdapterUsuariosRank.java
+++ app/src/main/java/com/projeto/naturezas/views/AdapterUsuariosRank.java
@@ -6,44 +6,68 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.R;
+import android.widget.TextView;
+
 import com.projeto.naturezas.models.Usuario;
 
 import java.util.List;
 
 public class AdapterUsuariosRank extends BaseAdapter {
 
-    private List<Usuario> listaUsuarios;
+    private List<Usuario> listaUsuario;
     private Context context;
 
-    public AdapterUsuariosRank(List<Usuario> listaUsuarios, Context context) {
-        this.listaUsuarios = listaUsuarios;
+    public AdapterUsuariosRank(List<Usuario> listaContatos, Context context) {
+        this.listaUsuario = listaContatos;
         this.context = context;
     }
 
     @Override
     public int getCount() {
-        return listaUsuarios.size();
+        return listaUsuario.size();
     }
-
 
     @Override
     public Usuario getItem(int position) {
-        return listaUsuarios.get(position);
+        return listaUsuario.get(position);
     }
 
     @Override
     public long getItemId(int position) {
-        return listaUsuarios.get(position).getId();
+        return listaUsuario.get(position).getId();
     }
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
-        // Inflar a view
-        View view = LayoutInflater.from(context).inflate(, parent, false);
 
+        // Inflar a view item
+        View view = LayoutInflater.from(context).inflate(R.layout.);
 
+        // Recuperar o contato
+        Usuario usuario = listaUsuario.get(position);
+
+        // Recuperar as views
+        TextView textViewNome = view.findViewById(R.id.item_contato_nome);
+        TextView textViewTelefone = view.findViewById(R.id.item_contato_telefone);
+        TextView textViewEnd = view.findViewById(R.id.item_contato_endcompleto);
+        TextView textViewcep = view.findViewById(R.id.item_contato_cep);
+
+        // Adicionar na view
+        textViewNome.setText(usuario.getNome());
+        textViewTelefone.setText(usuario.getNumero());
 
         return view;
     }
+
+    public void atualiza(List<Usuario> usuarios) {
+        listaUsuario.clear();
+        listaUsuario.addAll(usuarios);
+        notifyDataSetChanged();
+    }
+
+    public void remove(Usuario usuarios) {
+        listaUsuario.remove(usuarios);
+    }
+
 }
 

7b7507ceb42c2b5caed7a98953d791f3edb1f47a
Walisson José Lira de Araújo <walissonjoslir@gmail.com>
2019-12-05 23:47:18 -0300

    InicioRankAdapter Layout TelaJogo

---
+++ app/src/main/java/com/projeto/naturezas/views/AdapterUsuariosRank.java
@@ -0,0 +1,49 @@
+package com.projeto.naturezas.views;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.R;
+import com.projeto.naturezas.models.Usuario;
+
+import java.util.List;
+
+public class AdapterUsuariosRank extends BaseAdapter {
+
+    private List<Usuario> listaUsuarios;
+    private Context context;
+
+    public AdapterUsuariosRank(List<Usuario> listaUsuarios, Context context) {
+        this.listaUsuarios = listaUsuarios;
+        this.context = context;
+    }
+
+    @Override
+    public int getCount() {
+        return listaUsuarios.size();
+    }
+
+
+    @Override
+    public Usuario getItem(int position) {
+        return listaUsuarios.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return listaUsuarios.get(position).getId();
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        // Inflar a view
+        View view = LayoutInflater.from(context).inflate(, parent, false);
+
+
+
+        return view;
+    }
+}
+
